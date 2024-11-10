a7b90e0900d031037eafa7c9cdb07ce18adaee74
Rafael Menezes <rafa.rcm@hotmail.com>
2024-11-10 07:13:06 -0300

    Instalando React-Bootstrap

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import AtividadeForm from "./components/AtividadeForm";
 import AtividadeLista from "./components/AtividadeLista";
 import api from "./api/atividade";
 
 function App() {
+  const [show, setShow] = useState(false);
   const [atividades, setAtividades] = useState([]);
   const [atividade, setAtividade] = useState({ id: 0 });
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const pegaTodasAtividades = async () => {
     const response = await api.get("atividade");
@@ -68,6 +73,25 @@ function App() {
         deletarAtividade={deletarAtividade}
         pegarAtividade={pegarAtividade}
       />
+
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }

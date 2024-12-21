4dda47b58c055f57e0b3c9986bd4ab48590016a1
Rafael Menezes <rafa.rcm@hotmail.com>
2024-12-21 16:33:07 -0300

    Novas Rotas

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -3,13 +3,15 @@ import Atividades from "./pages/atividades/Atividades";
 import { Switch, Route } from "react-router-dom";
 import Cliente from "./pages/clientes/Cliente";
 import Dashboard from "./pages/dashboard/Dashboard";
+import ClienteForm from "./pages/clientes/ClienteForm";
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" exact component={Dashboard} />
-      <Route path="/atividades" component={Atividades} />
-      <Route path="/clientes" component={Cliente} />
+      <Route path="/atividade/lista" component={Atividades} />
+      <Route path="/cliente/lista" component={Cliente} />
+      <Route path="/cliente/detalhe" component={ClienteForm} />
     </Switch>
   );
 }
--- front/pro-atividade-app/src/components/Menu.jsx
+++ front/pro-atividade-app/src/components/Menu.jsx
@@ -11,10 +11,14 @@ export default function Menu() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link activeClassName="active" as={NavLink} to="/clientes">
+            <Nav.Link activeClassName="active" as={NavLink} to="/cliente/lista">
               Clientes
             </Nav.Link>
-            <Nav.Link activeClassName="active" as={NavLink} to="/atividades">
+            <Nav.Link
+              activeClassName="active"
+              as={NavLink}
+              to="/atividade/lista"
+            >
               Atividades
             </Nav.Link>
           </Nav>
